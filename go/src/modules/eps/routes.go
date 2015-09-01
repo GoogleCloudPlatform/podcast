060a22916833744f7f3ed4754d00bdf6adc878cb
Mark Mandel <markmandel@google.com>
2015-09-01 15:50:25 -0700

    Implemented the redirect.
    Change-Id: I6212742dba58270978fd0c2b314b38265c00d9ea

--- go/src/modules/eps/routes.go
+++ go/src/modules/eps/routes.go
@@ -5,18 +5,36 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
-const bucket = "https://storage.googleapis.com/eps"
+const bucket = "https://storage.googleapis.com/eps/"
+const episodeKey = "episode"
 
 //init
 func init() {
 	r := mux.NewRouter()
-	r.HandleFunc("/dl/{episode}", redirectEpisodeDownload)
+	r.HandleFunc("/dl/{"+episodeKey+"}", redirectEpisodeDownload)
+
+	http.Handle("/", r)
 }
 
 //redirectEpisodeDownload implements a redirect to downloading the
 //audio file that comes into the param {episode}.
 func redirectEpisodeDownload(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	vars := mux.Vars(r)
+	ep, ok := vars[episodeKey]
 
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
+
+	path := bucket + ep
+
+	log.Infof(ctx, "Redirecting file download to: %v", path)
+
+	http.Redirect(w, r, path, 307)
 }
